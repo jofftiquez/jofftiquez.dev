dfa867924f83474fff22bcebda7243a184b044a6
Jofferson Ramirez Tiquez <jofftiquez@gmail.com>
2016-04-15 11:27:59 +0800

    Meh

--- modules/main/controllers/main-controller.js
+++ modules/main/controllers/main-controller.js
@@ -6,7 +6,12 @@ theGreenFox.controller('mainController',
 		message: "Welcome to jofftiquez.com \n\nType '--help' to see available commands. \n\nIf you are not familiar with command line you can swith to"
 	}
 
-	$scope.handleCommand = function () {
+	$scope.commandEntered = {};
 
+	$scope.handleCommand = function () {
+		$('#directory-here').append(`<b class="color-green">guest@jofftiquez.com</b>
+		<b class="console-path"> / $</b><span class="entered-command">`+$scope.commandEntered.input+`</span><br>`);
+		$scope.commandEntered = {};
 	}
+
 }])
