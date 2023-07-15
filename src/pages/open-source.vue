d85561d7f33de32726c3d818527caf427932b9b7
Joff Tiquez <jofftiquez@gmail.com>
2023-07-15 12:17:16 +0800

    📝 chore(open-source.vue): add Open Source page 🎉 feat(open-source.vue): create Open Source page with information about various open source projects and social media links The Open Source page is added, which displays information about several open source projects and social media links. The page includes details about Vue Stripe, Open Source Software PH, Faker.js UI, Paymongo, and Vue HTML To Paper. It also provides links to their respective websites and GitHub repositories. Additionally, the page includes links to follow the author on GitHub, Twitter, and Facebook. Lastly, there is a section for sponsors, with a link to sponsor the author on GitHub.

---
+++ src/pages/open-source.vue
@@ -0,0 +1,104 @@
+<template>
+  <main class="mx-auto md:max-w-3xl px-5 py-5 md:py-7 min-h-screen">
+    <h1 class="text-4xl mb-10">Open Source</h1>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Vue Stripe</h2>
+      <p>A collection of component and plugins for Stripe</p>
+      <div class="flex gap-2">
+        <a href="https://vuestripe.com" target="_blank" class="underline underline-offset-2 hover:text-primary">Website</a>
+        <a href="https://github.com/vue-stripe/vue-stripe" target="_blank" class="underline underline-offset-2 hover:text-primary">1K stars on GitHub</a>
+      </div>
+    </section>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Open Source Software PH</h2>
+      <p>The leading Open Source Software Community in the Philippines</p>
+      <div class="flex gap-2">
+        <a href="https://ossph.org" target="_blank" class="underline underline-offset-2 hover:text-primary">Website</a>
+        <a href="https://blog.ossph.org" target="_blank" class="underline underline-offset-2 hover:text-primary">Blog</a>
+      </div>
+    </section>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Faker.js UI</h2>
+      <p>A simple and intuitive UI for Faker.js</p>
+      <div class="flex gap-2">
+        <a href="https://fakerjsui.com/" target="_blank" class="underline underline-offset-2 hover:text-primary">Website</a>
+        <a href="https://github.com/jofftiquez/faker-js-ui" target="_blank" class="underline underline-offset-2 hover:text-primary">185 stars on GitHub</a>
+      </div>
+    </section>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Paymongo</h2>
+      <p>A lightweight Node.js client for Paymongo API</p>
+      <div class="flex gap-2">
+        <a href="https://paymongo.ossph.org/" target="_blank" class="underline underline-offset-2 hover:text-primary">Website</a>
+        <a href="https://github.com/OSSPhilippines/paymongo" target="_blank" class="underline underline-offset-2 hover:text-primary">84 stars on GitHub</a>
+      </div>
+    </section>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Vue HTML To Paper</h2>
+      <p>A Vue plugin for printing html elements</p>
+      <div class="flex gap-2">
+        <a href="https://mycurelabs.github.io/vue-html-to-paper/" target="_blank" class="underline underline-offset-2 hover:text-primary">Website</a>
+        <a href="https://github.com/mycurelabs/vue-html-to-paper" target="_blank" class="underline underline-offset-2 hover:text-primary">288 stars on GitHub</a>
+      </div>
+    </section>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Follow me</h2>
+      <p>Follow me to be updated on my work and future projects</p>
+      <div class="flex gap-2">
+        <a href="https://github.com/jofftiquez" target="_blank" class="underline underline-offset-2 hover:text-primary">GitHub</a>
+        <a href="https://twitter.com/jrtiquez" target="_blank" class="underline underline-offset-2 hover:text-primary">Twitter</a>
+        <a href="https://facebook.com/jofftiquez" target="_blank" class="underline underline-offset-2 hover:text-primary">Facebook</a>
+      </div>
+    </section>
+    <section class="mb-8">
+      <h2 class="text-xl mb-2">Sponsors 🩷</h2>
+      <p>If you liked my projects or just simply want to support me, you can sponsor me on GitHub. Thank you 🙏</p>
+      <div class="flex gap-2">
+        <a href="https://github.com/sponsors/jofftiquez" target="_blank" class="underline underline-offset-2 hover:text-primary">GitHub Sponsors</a>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import { definePageMeta } from '#imports';
+import { useHead } from '#app';
+import ogBanner from '../assets/images/og-banner.png';
+
+export default {
+  setup () {
+    definePageMeta({
+      layout: 'default',
+    });
+
+    const metaTitle = 'Open Source - Joff Tiquez';
+    const metaDescription = 'A collection of my open source projects';
+
+    useHead({
+      title: metaTitle,
+      description: metaDescription,
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      ],
+      meta: [
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          property: 'og:image',
+          content: ogBanner,
+          title: metaTitle,
+          description: metaDescription,
+        },
+      ],
+    });
+
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+li::before {
+  content: '• ';
+}
+</style>
