8ba55f8c1e7b956fa9ad7b07cd1364cf865a2c3a
Joff Tiquez <jofftiquez@gmail.com>
2023-11-07 12:16:34 +0800

    🚀 chore(package.json): update Nuxt and DaisyUI versions 🖼️ feat(gui.vue): add image and adjust layout in GUI page
    The package.json file was updated to use the latest versions of Nuxt (3.6.5) and DaisyUI (3.9.4). This ensures that the project is using the most recent and stable versions of these dependencies.
    
    In the gui.vue file, an image was added to the page using the `<img>` tag. The layout of the GUI page was also adjusted to display the image on the left side and the content on the right side. This improves the visual presentation of the page and enhances the user experience.

--- src/pages/gui.vue
+++ src/pages/gui.vue
@@ -1,26 +1,33 @@
 <template>
 
-  <main class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-2xl">
-        <h1 data-aos="fade-down" class="text-4xl md:text-7xl font-bold mb-6">Hi 👋</h1>
-        <h1 data-aos="fade-down" class="text-4xl md:text-4xl font-bold mb-6">I'm Joff Tiquez</h1>
-        <p class="mb-10">
-          I am a web developer from Manila 🇵🇭 For the past decade I am mostly focusing on web development using JavaScript.
-        </p>
-        <div class="grid grid-flow-col gap-4">
-          <template v-for="(link, index) in socialLinks" :key="index">
-            <div
-              data-aos="fade-in"
-              class="tooltip tooltip-bottom"
-              :data-aos-delay="index * 200"
-              :data-tip="link.name"
-            >
-              <a :href="link.url" target="_blank">
-                <i :class="`${link.icon} text-4xl`"></i>
-              </a>
-            </div>
-          </template>
+  <main class="hero min-h-screen bg-base-100">
+    <div class="grid grid-cols-2 h-screen">
+
+      <div class="flex relative justify-center">
+        <img class="w-[500px] bottom-0 absolute" src="../assets/images/jofftiquez-compressed.png" alt="">
+      </div>
+
+      <div class="max-w-2xl ">
+        <div class="flex flex-col justify-center h-screen">
+          <h1 data-aos="fade-down" class="text-4xl md:text-7xl font-bold mb-6">Hi 👋</h1>
+          <h1 data-aos="fade-down" class="text-4xl md:text-4xl font-bold mb-6">I'm Joff Tiquez</h1>
+          <p class="mb-10">
+            I am a web developer from Manila 🇵🇭 For the past decade I am mostly focusing on web development using JavaScript.
+          </p>
+          <div class="flex flex-wrap gap-8">
+            <template v-for="(link, index) in socialLinks" :key="index">
+              <div
+                data-aos="fade-in"
+                class="tooltip tooltip-bottom"
+                :data-aos-delay="index * 200"
+                :data-tip="link.name"
+              >
+                <a :href="link.url" target="_blank">
+                  <i :class="`${link.icon} text-4xl`"></i>
+                </a>
+              </div>
+            </template>
+          </div>
         </div>
       </div>
     </div>
