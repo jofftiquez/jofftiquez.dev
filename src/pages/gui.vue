77719a979dda87ba7c9210a5e289f8bac52b7882
Joff Tiquez <jofftiquez@gmail.com>
2024-02-02 22:18:31 +0800

    🔧 chore(gui.vue): refactor VANTA effect initialization to improve code readability and maintainability 💄 style(gui.vue): remove commented out code that is no longer needed The VANTA effect initialization code has been refactored to improve code readability and maintainability. The switch statement has been replaced with individual function calls for each effect, making it easier to understand and modify. Additionally, commented out code that is no longer needed has been removed to clean up the file.

--- src/pages/gui.vue
+++ src/pages/gui.vue
@@ -1,11 +1,135 @@
+<script setup>
+import { definePageMeta, useSeoMeta, onMounted } from '#imports';
+import { SOCIAL_LINKS } from '~/constants';
+import ogBanner from '../assets/images/og-banner.png';
+
+definePageMeta({
+  layout: 'default',
+  pageName: 'GUI (Graphical User Interface)',
+  order: 1,
+});
+
+const metaTitle = 'Home - Joff Tiquez';
+const metaDescription = 'Web Developer from 🇵🇭';
+
+useSeoMeta({
+  title: metaTitle,
+  ogTitle: metaTitle,
+  description: metaDescription,
+  ogDescription: metaDescription,
+  image: ogBanner,
+  ogImage: ogBanner,
+  url: 'https://jofftiquez.com/gui',
+  ogUrl: 'https://jofftiquez.com/gui',
+  twitterCard: 'summary_large_image',
+});
+
+const socialLinks = SOCIAL_LINKS;
+
+onMounted(() => {
+  const effects = [
+    'fog',
+    'clouds',
+    'net',
+    'trunk',
+    'halo',
+  ];
+
+  const effect = effects[Math.floor(Math.random() * effects.length)];
+
+  // TODO: standardize a method to load VANTA effect
+  switch (effect) {
+    case 'fog':
+      globalThis.VANTA && globalThis.VANTA.FOG({
+        el: '#gui-page',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0xffffff,
+        midtoneColor: 0x343131,
+        lowlightColor: 0x35333e,
+        baseColor: 0x0,
+        blurFactor: 0.59,
+        speed: 3.00,
+        zoom: 0.40,
+      });
+      break;
+    case 'clouds':
+      globalThis.VANTA && globalThis.VANTA.CLOUDS({
+        el: '#gui-page',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        skyColor: 0x0,
+        cloudColor: 0x4e4e52,
+        cloudShadowColor: 0x0,
+        sunColor: 0x0,
+        sunGlareColor: 0x0,
+        sunlightColor: 0x0,
+        speed: 2.00,
+      });
+      break;
+    case 'net':
+      globalThis.VANTA && globalThis.VANTA.NET({
+        el: '#gui-page',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xffffff,
+        backgroundColor: '#181e23',
+        spacing: 20.00,
+      });
+      break;
+    case 'trunk':
+      globalThis.VANTA && globalThis.VANTA.TRUNK({
+        el: '#gui-page',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x433d3e,
+        backgroundColor: 0x191e22,
+        spacing: 1.50,
+        chaos: 3.00,
+      });
+      break;
+    case 'halo':
+      globalThis.VANTA && globalThis.VANTA.HALO({
+        el: '#gui-page',
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        baseColor: 0x0,
+        backgroundColor: '#181e23',
+        amplitudeFactor: 3.00,
+        xOffset: 0.15,
+        size: 0.60,
+      });
+  }
+});
+</script>
+
 <template>
 
-  <main class="hero min-h-screen bg-base-100">
+  <main id="gui-page" class="hero min-h-screen bg-base-100">
     <div class="grid grid-cols-1 md:grid-cols-2 h-screen p-4 md:p-0">
 
-      <div class="hidden md:flex relative justify-center">
+      <!-- <div class="hidden md:flex relative justify-center">
         <img class="w-[500px] bottom-0 absolute" src="../assets/images/jofftiquez-compressed.png" alt="">
-      </div>
+      </div> -->
 
       <div class="max-w-2xl ">
         <div class="flex flex-col justify-center h-screen">
@@ -34,38 +158,3 @@
   </main>
 
 </template>
-
-<script>
-import { definePageMeta, useSeoMeta } from '#imports';
-import { SOCIAL_LINKS } from '~/constants';
-import ogBanner from '../assets/images/og-banner.png';
-
-export default {
-  setup () {
-    definePageMeta({
-      layout: 'default',
-      pageName: 'GUI (Graphical User Interface)',
-      order: 1,
-    });
-
-    const metaTitle = 'Home - Joff Tiquez';
-    const metaDescription = 'Web Developer from 🇵🇭';
-
-    useSeoMeta({
-      title: metaTitle,
-      ogTitle: metaTitle,
-      description: metaDescription,
-      ogDescription: metaDescription,
-      image: ogBanner,
-      ogImage: ogBanner,
-      url: 'https://jofftiquez.com/gui',
-      ogUrl: 'https://jofftiquez.com/gui',
-      twitterCard: 'summary_large_image',
-    });
-
-    return {
-      socialLinks: SOCIAL_LINKS,
-    };
-  },
-};
-</script>
